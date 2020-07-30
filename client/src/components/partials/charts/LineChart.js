24e95c39a0e6079f121fc25573cd89349af5978d
Tamara Smith <tdesmith@shaw.ca>
2020-07-29 18:22:44 -0700

    working on charts

--- client/src/components/partials/charts/LineChart.js
+++ client/src/components/partials/charts/LineChart.js
@@ -18,7 +18,7 @@ function LineChart(props) {
         rotate = 30;
         offset = 10;
         ticks = 24;
-        return d3.timeFormat("%I %p")
+        return d3.timeFormat("%I %p");
       case 7:
         rotate = 0;
         offset = 0;
@@ -28,13 +28,11 @@ function LineChart(props) {
         offset = 0;
         rotate = 0;
         ticks = 31;
-
         return d3.timeFormat("%d");
       case 365:
         offset = 0;
         rotate = 0;
         ticks = 12;
-
         return d3.timeFormat("%b");
       default:
         return d3.timeFormat("%I %p");
@@ -72,7 +70,6 @@ function LineChart(props) {
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-
     // Add Y axis
     const y = d3.scaleLinear()
       .domain([setMax(data), (type === 'temperature' ? 0 : setMin(data))])
@@ -88,12 +85,7 @@ function LineChart(props) {
       .domain([start, new Date(data[data.length - 1].time)])
       .range([0, width]);
 
-    // create line
-    const lineGenerator = d3.line()
-      .defined(function (d) { return d.value !== null; })
-      .x(function (d) { return x(new Date(d.time)); })
-      .y(function (d) { return y(d.value); })
-
+   
     // add the X Axis
     svg.append("g")
       .attr("transform", `translate(0, ${height})`)
@@ -116,39 +108,18 @@ function LineChart(props) {
         .text(data.y))
 
 
-    var parseDate = d3.timeFormat("%m/%e/%Y").parse,
-      bisectDate = bisectDate = d3.bisector(function (d) { return d.time; }).left,
-      formatValue = d3.format(","),
-      dateFormatter = d3.timeFormat("%m/%d/%y");
+    // create line
+    const lineGenerator = d3.line()
+      .defined(function (d) { return d.value !== null; })
+      .x(function (d) { return x(new Date(d.time)); })
+      .y(function (d) { return y(d.value); })
 
+    // main Path
     svg.append("path")
       .datum(data)
       .attr("class", type === 'temperature' ? "max-line" : 'line')
       .attr("d", lineGenerator)
-      .on("mouseover", function () {
-        const xMouse = x.invert(d3.mouse(this)[0]);
-        const date = new Date(xMouse).toISOString()
-        const i = bisectDate(data, date, 1);
-        const reading = data[i - 1];
-        const time = new Date(reading.time);
-        const formate = dateformate()
-        const hour = time.getHours();
-        const newTime = formate(time)
-        tooltip.text(`Time: ${(newTime)} ${'\n'} value:  ${reading.value}`)
-        return tooltip.style("visibility", "visible")
-      })
-      .on("mouseout", () => setTimeout(() => {
-        tooltip.style("visibility", "hidden")
-      }, 3000))
 
-
-    const tooltip = d3.select("body")
-      .append("div")
-      .attr("class", "tooltip")
-      .style("position", "absolute")
-      .style("z-index", 10)
-      .style("visibility", "hidden")
-      .text("Simple text");
 
     // MIN Pathes  
     if (data[0].min || data[0].min === null) {
@@ -156,44 +127,87 @@ function LineChart(props) {
         .defined(function (d) { return d.value !== null; })
         .x(function (d) { return x(new Date(d.time)); })
         .y(function (d) { return y(d.min); });
-
       svg.append("path")
-        .data([data])
+        .datum([data])
         .attr("class", "min-line")
         .attr("d", lineLowGenerator)
-        .text(d => d.value)
-        .on("mouseover", function () {
-          const xMouse = x.invert(d3.mouse(this)[0]);
-          const date = new Date(xMouse).toISOString()
-          const i = bisectDate(data, date, 1);
-          const reading = data[i - 1];
-          const time = new Date(reading.time);
-          const formate = dateformate()
-          const hour = time.getHours();
-          const newTime = formate(time)
-          tooltip.text(`Time: ${(newTime)} ${'\n'} value:  ${reading.value}`)
-          return tooltip.style("visibility", "visible")
-        })
-        .on("mouseout", () => setTimeout(() => {
-          tooltip.style("visibility", "hidden")
-        }, 3000))
-
-
     }
 
-    // const mouseOver = function() {
-    //   const xMouse = x.invert(d3.mouse(this)[0]);
-    //   const date = new Date(xMouse).toISOString()
-    //   const i = bisectDate(data, date, 1);
-    //   const reading = data[i - 1];
-    //   const time = new Date(reading.time);
-    //   const hour = time.getHours();
-    //   tooltip.text(`Time: ${(hour)} \n value:  ${reading.value}`)
-    //   return tooltip.style("visibility", "visible")
-    // })
+
+    // Tooltip
+    const bisectDate = d3.bisector(function (d) { return d.time; }).left
+
+    const tooltip = svg.append("g")
+
+    const tooltipMessage = (reading) => {
+      const formate = dateformate();
+      let result = "";
+      (Object.keys(reading)).forEach(key => {
+        if (key === 'time') {
+          const time = formate(new Date(reading[key]));
+          result += `${key}: ${time} \n`;
+        } else {
+          result += `${key}: ${reading[key]} \n`;
+        }
+      })
+      return result;
+    }
+
+
+    const callout = (g, value) => {
+      if (!value) return g.style("display", "none");
+
+      g
+        .style("display", null)
+        .style("pointer-events", "none")
+        .style("font", "15px sans-serif")
+
+      const path = g.selectAll("path")
+        .data([null])
+        .join("path")
+        .style("fill", '#fffcf2')
+        .attr("stroke", "black");
+
+      const text = g.selectAll("text")
+        .data([null])
+        .join("text")
+        .call(text => text
+          .selectAll("tspan")
+          .data((value + "").split(/\n/))
+          .join("tspan")
+          .attr("x", 0)
+          .attr("y", (d, i) => `${i * 1.1}em`)
+          .style("font-weight", (_, i) => i ? null : "bold")
+          .text(d => d));
+
+      const { y, width: w, height: h } = text.node().getBBox();
+
+      text.attr("transform", `translate(${-w / 2},${15 - y})`);
+      path.attr("d", `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`);
+    }
 
 
 
+    svg.on("mousemove touchmove", function () {
+
+      const xMouse = x.invert(d3.mouse(this)[0]);
+      const date = new Date(xMouse).toISOString()
+      const i = bisectDate(data, date, 1);
+      const reading = data[i - 1];
+      const messages = tooltipMessage(reading)
+
+      tooltip
+        .attr("transform", `translate(${x(new Date(reading.time))},${y(reading.value)})`)
+        .call(callout, `${messages}`)
+    });
+
+    svg.on('mouseout', ()=>{
+      setTimeout(() => {
+        tooltip
+        .call(callout, null)
+      }, 2000);
+
+    })
   }
 
 
